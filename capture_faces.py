cf43a3556868291380f684b9937d4770bb80d4b3
ncentola <ncentola@iloan.com>
2017-03-06 00:54:07 +0000

    will capture face and put in folder name passed as sys.argv[1]

--- capture_faces.py
+++ capture_faces.py
@@ -15,6 +15,7 @@ import hardware
 import config
 import face
 
+person_name = sys.argv[1]
 
 # Prefix for positive training image filenames.
 POSITIVE_FILE_PREFIX = 'positive_'
@@ -31,14 +32,16 @@ def is_letter_input(letter):
 
 if __name__ == '__main__':
 	camera = config.get_camera()
-	box = hardware.Box()
+	#box = hardware.Box()
+	
+	folder_path = os.path.join('/home/pi/faces/', person_name) 
 	# Create the directory for positive training images if it doesn't exist.
-	if not os.path.exists(config.POSITIVE_DIR):
-		os.makedirs(config.POSITIVE_DIR)
+	if not os.path.exists(folder_path):
+		os.makedirs(folder_path)
 	# Find the largest ID of existing positive images.
 	# Start new images after this ID value.
-	files = sorted(glob.glob(os.path.join(config.POSITIVE_DIR, 
-		POSITIVE_FILE_PREFIX + '[0-9][0-9][0-9].pgm')))
+	files = sorted(glob.glob(os.path.join(folder_path, 
+		'[0-9][0-9][0-9].pgm')))
 	count = 0
 	if len(files) > 0:
 		# Grab the count from the last filename.
@@ -48,23 +51,31 @@ if __name__ == '__main__':
 	print 'Press Ctrl-C to quit.'
 	while True:
 		# Check if button was pressed or 'c' was received, then capture image.
-		if box.is_button_up() or is_letter_input('c'):
-			print 'Capturing image...'
-			image = camera.read()
-			# Convert image to grayscale.
-			image = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
-			# Get coordinates of single face in captured image.
-			result = face.detect_single(image)
-			if result is None:
-				print 'Could not detect single face!  Check the image in capture.pgm' \
-					  ' to see what was captured and try again with only one face visible.'
-				continue
-			x, y, w, h = result
-			# Crop image as close as possible to desired face aspect ratio.
-			# Might be smaller if face is near edge of image.
-			crop = face.crop(image, x, y, w, h)
-			# Save image to file.
-			filename = os.path.join(config.POSITIVE_DIR, POSITIVE_FILE_PREFIX + '%03d.pgm' % count)
-			cv2.imwrite(filename, crop)
-			print 'Found face and wrote training image', filename
-			count += 1
+		#if box.is_button_up() or is_letter_input('c'):
+		print 'Capturing image...'
+		image = camera.read()
+		# Convert image to grayscale.
+		image = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+		
+		cv2.imshow('Frame', image)
+                cv2.waitKey(0)
+
+		# Get coordinates of single face in captured image.
+		result = face.detect_face(image, single = True)
+		if result is None:
+			print 'Could not detect single face!  Check the image in capture.pgm' \
+				  ' to see what was captured and try again with only one face visible.'
+			continue
+		x, y, w, h = result
+		# Crop image as close as possible to desired face aspect ratio.
+		# Might be smaller if face is near edge of image.
+		crop = face.crop(image, x, y, w, h)
+		# Save image to file.
+		filename = os.path.join(folder_path, '%s.pgm' % (count))
+
+		#cv2.imshow('Frame', image)
+		#cv2.waitKey(0)
+		
+		cv2.imwrite(filename, crop)
+		print 'Found face and wrote training image', filename
+		count += 1
