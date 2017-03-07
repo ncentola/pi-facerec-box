f180349b4fae9e29f5d9b62c82a51c03ec32352e
ncentola <ncentola@iloan.com>
2017-03-07 01:30:28 +0000

    better way to grab last file

--- capture_faces.py
+++ capture_faces.py
@@ -40,12 +40,18 @@ if __name__ == '__main__':
 		os.makedirs(folder_path)
 	# Find the largest ID of existing positive images.
 	# Start new images after this ID value.
-	files = sorted(glob.glob(os.path.join(folder_path, 
-		'[0-9][0-9][0-9].pgm')))
+	#files = sorted(glob.glob(os.path.join(folder_path, 
+	#	'[0-9][0-9][0-9].pgm')))
+	
+	#eventually add some logic to only find .pgm files
+	file_names = []
+	files = os.listdir(folder_path)
 	count = 0
 	if len(files) > 0:
-		# Grab the count from the last filename.
-		count = int(files[-1][-7:-4])+1
+		for file in files:
+			file_names.append(int(file.replace('.pgm', '')))
+		count = max(file_names)
+
 	print 'Capturing positive training images.'
 	print 'Press button or type c (and press enter) to capture an image.'
 	print 'Press Ctrl-C to quit.'
@@ -56,26 +62,21 @@ if __name__ == '__main__':
 		image = camera.read()
 		# Convert image to grayscale.
 		image = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
-		
-		cv2.imshow('Frame', image)
-                cv2.waitKey(0)
-
+				
 		# Get coordinates of single face in captured image.
 		result = face.detect_face(image, single = True)
-		if result is None:
-			print 'Could not detect single face!  Check the image in capture.pgm' \
-				  ' to see what was captured and try again with only one face visible.'
-			continue
-		x, y, w, h = result
-		# Crop image as close as possible to desired face aspect ratio.
-		# Might be smaller if face is near edge of image.
-		crop = face.crop(image, x, y, w, h)
-		# Save image to file.
-		filename = os.path.join(folder_path, '%s.pgm' % (count))
+		if result is not None:
+                        x, y, w, h = result
+                        crop = face.crop(image, x, y, w, h)
+                	# Save image to file.
+                	filename = os.path.join(folder_path, '%s.pgm' % (count))
 
-		#cv2.imshow('Frame', image)
-		#cv2.waitKey(0)
+                       	cv2.imwrite(filename, crop)
+                	print 'Found face and wrote training image', filename
+                	count += 1
+			cv2.rectangle(image, (x, y), (x+w, y+h), (255, 255, 0))
+
+                cv2.imshow('Frame', image)
+                cv2.waitKey(1) & 0xFF
+
 		
-		cv2.imwrite(filename, crop)
-		print 'Found face and wrote training image', filename
-		count += 1
