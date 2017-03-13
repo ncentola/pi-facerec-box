9835c58c037208e2774808471dbadb875e3b74d7
ncentola <ncentola@iloan.com>
2017-03-13 00:31:59 +0000

    log activity

--- box.py
+++ box.py
@@ -11,6 +11,7 @@ import pandas as pd
 import pigpio
 import RPi.GPIO as GPIO
 import time
+import datetime
 
 GPIO.setmode(GPIO.BCM)
 
@@ -20,7 +21,12 @@ pi = pigpio.pi()
 #p = GPIO.PWM(22, 50)
 #p.start(7.5)
 
-def dispense(id, name, confidence):
+def log(engine, user_id, confidence):
+	row = [(user_id, confidence, datetime.datetime.now())]
+	df = pd.DataFrame.from_records(row, columns = ['user_id', 'confidence', 'created_at'])
+	df.to_sql('activity', engine, if_exists = 'append')
+	
+def dispense(name):
         print 'Dispensing for ' + name
         #GPIO.output(22, 1)
         #time.sleep(0.0015)
@@ -49,46 +55,53 @@ if __name__ == '__main__':
 	users = pd.read_sql('users', engine)
 
 	while True:
-		image = camera.read()
-		# Convert image to grayscale.
-		image = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+		try:
+			image = camera.read()
+			# Convert image to grayscale.
+			image = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
 		
-		# Get coordinates of single face in captured image.
-		faces = face.detect_face(image, single = False)
+			# Get coordinates of single face in captured image.
+			faces = face.detect_face(image, single = False)
 		
-		if faces is not None:
-                        for (x, y, w, h) in faces:
-                                cv2.rectangle(image, (x, y), (x+w, y+h), (255, 255, 0))
+			if faces is not None:
+                        	for (x, y, w, h) in faces:
+                                	cv2.rectangle(image, (x, y), (x+w, y+h), (255, 255, 0))
 
 
-		if faces is not None:
+			if faces is not None:
 			
-			for facez in faces:
-				x, y, w, h = facez
+				for facez in faces:
+					x, y, w, h = facez
+					
+					## Crop and resize image to face.
+					crop = face.resize(face.crop(image, x, y, w, h))
 				
-				## Crop and resize image to face.
-				crop = face.resize(face.crop(image, x, y, w, h))
+					## Test face against model.
+					user_id, confidence = model.predict(crop)
 				
-				## Test face against model.
-				id, confidence = model.predict(crop)
+					name = users['name'].loc[users['user_id'] == user_id]
+					print name
+					print type(name)
+					name = str(name)
+					#print name.at[0,1]
+					cv2.rectangle(image, (x, y), (x+w, y+h), (255, 255, 0))
+					cv2.putText(image, name, (x, y+h+20), cv2.FONT_HERSHEY_SIMPLEX, 0.4, (255, 255, 0), 2)
 				
-				name = str(users['name'].loc[users['id'] == id])
-				
-				cv2.rectangle(image, (x, y), (x+w, y+h), (255, 255, 0))
-				cv2.putText(image, name, (x, y+h+20), cv2.FONT_HERSHEY_SIMPLEX, 0.4, (255, 255, 0), 2)
-				try:
 					print(is_button_pressed())
 					if not is_button_pressed():
-						dispense(id, name, confidence)
-				except KeyboardInterrupt:
-					pi.stop()
-					GPIO.cleanup()		
-				print name
-				print confidence
-				
-                cv2.imshow('Frame', image)
-                cv2.waitKey(1) & 0xFF
+						log(engine, user_id, confidence)
+						dispense(name)
+						
 
+					print name
+					print confidence
+				
+                	cv2.imshow('Frame', image)
+                	cv2.waitKey(1) & 0xFF
+
+		except KeyboardInterrupt:
+			pi.stop()
+			GPIO.cleanup()
 
 	
 		#print 'Predicted {0} face with confidence {1} (lower is more confident).'.format(
