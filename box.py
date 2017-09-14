487272bfbc0231d7a36371ac3f95f4da9c71f932
ncentola <ncentola@iloan.com>
2017-09-14 15:54:46 -0400

    save images for later classification, add some secondary detector for dispensing

--- box.py
+++ box.py
@@ -6,7 +6,7 @@ import cv2
 import config
 import face
 import hardware
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
 import pandas as pd
 import pigpio
 import RPi.GPIO as GPIO
@@ -17,18 +17,33 @@ import random
 GPIO.setmode(GPIO.BCM)
 
 GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(5, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 #GPIO.setup(22, GPIO.OUT)
 pi = pigpio.pi()
 #p = GPIO.PWM(22, 50)
 #p.start(7.5)
 
-def log(user_id, confidence, motor, engine):
-	row = [(user_id, confidence, motor, datetime.datetime.now())]
-	df = pd.DataFrame.from_records(row, columns = ['user_id', 'confidence', 'motor',  'created_at'])
-	df.to_sql('activity', engine, if_exists = 'append')
+def log(user_id, confidence, motor, engine, crop):
+		
+	if engine.dialect.has_table(engine, 'activity'):
+		sql = text('SELECT activity_id FROM activity ORDER BY created_at DESC LIMIT 1')
+		result = engine.execute(sql)	
+		l = []
+		for thing in result:
+			l.append(thing)
+		activity_id = l[0]['activity_id'] + 1
 	
-def dispense(name, user_id, confidence, engine):
+	else:
+		activity_id = 1000
+
+	row = [(activity_id, user_id, confidence, motor, datetime.datetime.now())]
+        df = pd.DataFrame.from_records(row, columns = ['activity_id', 'user_id', 'confidence', 'motor',  'created_at'])
+        df.to_sql('activity', engine, if_exists = 'append')
+	file_name_path = 'debug_images/' + str(activity_id) + '.png'
+	cv2.imwrite(file_name_path, crop)
+
+	
+def dispense(name, user_id, confidence, engine, crop):
         print 'Dispensing for ' + name
 	
 	if random.uniform(0, 1.0) < 1:
@@ -38,10 +53,10 @@ def dispense(name, user_id, confidence, engine):
                 #        pi.set_servo_pulsewidth(22, 2000)
                 #        time.sleep(.05)
 
-                pi.set_servo_pulsewidth(22, 1800)
-                time.sleep(0.3)
-                pi.set_servo_pulsewidth(22, 1000)
-		log(user_id, confidence, 1, engine)
+                pi.set_servo_pulsewidth(17, 1400)
+                time.sleep(0.5)
+                pi.set_servo_pulsewidth(17, 1000)
+		log(user_id, confidence, 1, engine, crop)
 
         else:
 		for i in range(1, 5):
@@ -52,14 +67,14 @@ def dispense(name, user_id, confidence, engine):
                 pi.set_servo_pulsewidth(27, 1000)
                 time.sleep(0.5)
                 pi.set_servo_pulsewidth(27, 0)
-		log(user_id, confidence, 1, engine)
+		log(user_id, confidence, 1, engine, crop)
 
-	pi.set_servo_pulsewidth(22, 0)
-	pi.set_servo_pulsewidth(27, 0)
+	#pi.set_servo_pulsewidth(17, 0)
+	#pi.set_servo_pulsewidth(27, 0)
 	
 def is_button_pressed():
-	input_state = GPIO.input(17) and GPIO.input(18)
-	pi.write(17, 1)
+	input_state = GPIO.input(5) and GPIO.input(18)
+	pi.write(5, 1)
 	pi.write(18, 1)
 	return input_state
 
@@ -94,13 +109,21 @@ if __name__ == '__main__':
 		
 			# Get coordinates of single face in captured image.
 			faces = face.detect_face(image, single = False)
+			#faces = None
 			#standby_lights(i)
 			pi.write(23, 0)
 			pi.write(24, 0)
 			
 			if faces is not None:
+				image_copy = image.copy()
 				for (x, y, w, h) in faces:
                                		cv2.rectangle(image, (x, y), (x+w, y+h), (255, 255, 0))
+				
+				#smiles = face.detect_smile(image, single = False)
+				#if smiles is not None:
+				#	for (x, y, w, h) in smiles:
+                                #        	cv2.rectangle(image, (x, y), (x+w, y+h), (255, 255, 0))
+
 
 
 			if faces is not None:
@@ -110,8 +133,8 @@ if __name__ == '__main__':
 					x, y, w, h = facez
 					
 					## Crop and resize image to face.
-					crop = face.resize(face.crop(image, x, y, w, h))
-				
+					crop = face.resize(face.crop(image_copy, x, y, w, h))
+					
 					## Test face against model.
 					user_id, confidence = model.predict(crop)
 				
@@ -125,14 +148,14 @@ if __name__ == '__main__':
 					
 					if not is_button_pressed():
 						#log(engine, user_id, confidence)
-						dispense(name, user_id, confidence, engine)
+						dispense(name, user_id, confidence, engine, crop)
 						
 
 					print name
 					print confidence
 				
-                	#cv2.imshow('Frame', image)
-                	#cv2.waitKey(1) & 0xFF
+                	cv2.imshow('Frame', image)
+                	cv2.waitKey(1) & 0xFF
 			
 			i =  not i
 
